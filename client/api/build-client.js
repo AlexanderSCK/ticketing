41beb52ca826cbea650c5fb307135e453b991dde
Alexander Petrov <alexander.ivaylov.petrov@gmail.com>
2022-05-25 14:00:35 +0200

    Front-end

--- client/api/build-client.js
+++ client/api/build-client.js
@@ -1,7 +1,6 @@
 import axios from 'axios';
 
 export default ({ req }) => {
-  if (typeof window === 'undefined') {
     // We are on the server
 
     return axios.create({
@@ -9,10 +8,5 @@ export default ({ req }) => {
         'http://www.ticketing-app-prod.lol/',
       headers: req.headers,
     });
-  } else {
-    // We must be on the browser
-    return axios.create({
-      baseUrl: '/',
-    });
-  }
+ 
 };
