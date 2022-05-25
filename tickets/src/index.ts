b500e103ecd01812a22c2f760c9eaac6d101b4e8
Alexander Petrov <alexander.ivaylov.petrov@gmail.com>
2022-05-25 11:35:57 +0200

    added set up

--- tickets/src/index.ts
+++ tickets/src/index.ts
@@ -7,7 +7,7 @@ import { OrderCancelledListener } from './events/listeners/order-cancelled-liste
 
 const start = async () => {
   if (!process.env.JWT_KEY) {
-    throw new Error('JWT_KEY must be defined');
+    throw new Error('JWT_KEY must be defineddd');
   }
   if (!process.env.MONGO_URI){
     throw new Error('MONGO_URI must be defined');
